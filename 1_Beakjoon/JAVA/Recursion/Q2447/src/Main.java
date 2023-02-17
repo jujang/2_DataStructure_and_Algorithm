74ed0d467f6224d59c67129033f2bcc1755a4218
jujang <jeongookjang@naver.com>
2023-02-18 03:24:09 +0900

    codingtest study - BeakJoon

--- 1_Beakjoon/JAVA/Recursion/Q2447/src/Main.java
+++ 1_Beakjoon/JAVA/Recursion/Q2447/src/Main.java
@@ -10,42 +10,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
-        star(N);
+        
 
         bw.flush();
         bw.close();
     }
 
-    public static void star(int N) throws IOException{
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < N; i++){
-            if(i == N/2) {
-                pointingEmpty(i);
-            } else {
-                pointingStar(i);
-            }
-            if((i+1)%Math.sqrt(N) == 0){
-                bw.write("\n");
-            }
-        }
-
-    }
-
-    public static void pointingStar(int N) throws IOException{
-//        if()
-        bw.write("*");
-    }
-
-    public static void pointingEmpty(int N) throws IOException{
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(" ");
-    }
 }
