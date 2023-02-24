321a7ca43d6c5e30ac367986b1e1df11e21928a3
jujang <jeongookjang@naver.com>
2023-02-25 04:14:45 +0900

    codingtest study - BeakJoon

---
+++ 1_Beakjoon/JAVA/PrefixSum/Q11659/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        bw.flush();
+        bw.close();
+    }
+
+
+}
