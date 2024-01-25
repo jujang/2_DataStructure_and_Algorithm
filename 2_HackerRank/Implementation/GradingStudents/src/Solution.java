72439ea410eebab816d2a852b016a523b3d598d4
jujang <jeongookjang@naver.com>
2024-01-25 10:58:55 +0900

    Algorithm: HackerRank - GradingStudents in Implementation problem solved

---
+++ 2_HackerRank/Implementation/GradingStudents/src/Solution.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int i = 0; i < grades.size(); i++){
+            int value = grades.get(i);
+            if(value < 38) {
+                list.add(value);
+            } else if(value%5 == 4) {
+                list.add(value + 1);
+            } else if(value%5 == 3) {
+                list.add(value + 2);
+            } else {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
