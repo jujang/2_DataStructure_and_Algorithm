d911d47dc29c2c3845f74806315e7ef5ae0814b5
jujang <jeongookjang@naver.com>
2024-01-30 03:34:07 +0900

    Algorithm: HackerRank - TheHurdleRace in Implementation problem solved

---
+++ 2_HackerRank/Implementation/TheHurdleRace/src/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY height
+     */
+
+    public static int hurdleRace(int k, List<Integer> height) {
+        // Write your code here
+        // int oldK = height.get(0);
+        int curVal = 0;
+        int maxDiff = 0;
+
+        for(int i = 0; i < height.size(); i++){
+            // oldK = height.get(i-1);
+            curVal = height.get(i);
+            if(maxDiff < curVal) maxDiff = curVal;
+        }
+
+        if(maxDiff - k <= 0) return 0;
+        else return maxDiff-k;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
