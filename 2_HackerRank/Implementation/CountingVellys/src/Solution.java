d4c74a9763137e6345efe914429dace174515869
jujang <jeongookjang@naver.com>
2024-01-30 16:38:34 +0900

    Algorithm: HackerRank - CountingVelleys in Implementation problem solved

---
+++ 2_HackerRank/Implementation/CountingVellys/src/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int height = 0;
+        int cnt = 0;
+        for(int i = 0; i < path.length(); i++){
+            if(path.charAt(i) == 'U') {
+                height++;
+                if(height == 0) cnt++;
+            } else height--;
+
+        }
+
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
