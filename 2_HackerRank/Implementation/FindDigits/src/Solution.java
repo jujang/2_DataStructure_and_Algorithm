edc27f07cdaa1d8d2d7c7ced695735973b754ee7
jujang <jeongookjang@naver.com>
2024-01-30 16:05:09 +0900

    Algorithm: HackerRank - FindDigits in Implementation problem solved

---
+++ 2_HackerRank/Implementation/FindDigits/src/Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        // Write your code here
+        int val = n;
+        int cnt = 0;
+        while(val != 0){
+            if(val % 10 != 0) {
+                if(n%(val%10) == 0) cnt++;
+            }
+            val /= 10;
+        }
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
