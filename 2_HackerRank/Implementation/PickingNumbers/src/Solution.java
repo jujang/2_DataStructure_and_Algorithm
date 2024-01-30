0d4f2ca767ed9dfea48fef5ed91fe8fdb5a1198c
jujang <jeongookjang@naver.com>
2024-01-30 17:20:29 +0900

    Algorithm: HackerRank - PickingNumbers in Implementation problem solved

---
+++ 2_HackerRank/Implementation/PickingNumbers/src/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+
+        int[] list = new int[100];
+        for(int i = 0; i < a.size(); i++){
+            list[a.get(i)-1] = list[a.get(i)-1]+1;
+        }
+
+        int maxSum = 0;
+        for(int j = 0; j < a.size()-1; j++){
+            if(list[j] + list[j+1] > maxSum) maxSum = list[j] + list[j+1];
+        }
+
+        return maxSum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
