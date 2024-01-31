f4ee54efc611f3290454032396a19bac770b354d
jujang <jeongookjang@naver.com>
2024-01-31 14:00:24 +0900

    Algorithm: HackerRank - CutTheSticks in Implementation problem solved

---
+++ 2_HackerRank/Implementation/CutTheSticks/src/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> cutTheSticks(List<Integer> arr) {
+        // Write your code here
+        List<Integer> list = new ArrayList<Integer>();
+
+        Collections.sort(arr);
+        while(arr.size() != 0){
+            list.add(arr.size());
+            int minStick = arr.get(0);
+            for(int i = 0; i < arr.size(); i++){
+                arr.set(i, arr.get(i) - minStick);
+            }
+            while(arr.size() != 0 && arr.get(0) == 0){
+                arr.remove(0);
+            }
+        }
+        return list;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.cutTheSticks(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
