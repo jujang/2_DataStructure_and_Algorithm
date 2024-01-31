5564eacdb06f8d2583548342f09ea1e891d9f854
jujang <jeongookjang@naver.com>
2024-01-31 15:37:08 +0900

    Algorithm: HackerRank - JumpingOnTheClouds in Implementation problem solved

---
+++ 2_HackerRank/Implementation/JumpingOnTheClouds/src/Solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+        int cnt = 0;
+        for(int i = 0; i < c.size(); i++){
+            if(i+2 < c.size() && c.get(i+2) != 1){
+                cnt++;
+                i++;
+            } else if(i+1 < c.size()) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
