be3be6b4bacf6ac37e4c621cfbfbc18c6ba175e8
jujang <jeongookjang@naver.com>
2024-01-31 15:50:49 +0900

    Algorithm: HackerRank - EqualizeTheArray in Implementation problem solved

---
+++ 2_HackerRank/Implementation/EqualizeTheArray/src/Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<Integer> arr) {
+        // Write your code here
+        int cntEqual = 0;
+        for(int i = 1; i <= 100; i++){
+            int cnt = 0;
+            for(int j = 0; j < arr.size(); j++){
+                if(arr.get(j) == i){
+                    cnt++;
+                    if(cnt > cntEqual) cntEqual = cnt;
+                }
+            }
+        }
+        return arr.size() - cntEqual;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
