adb2b694108fce63afe871a032246727f3a67803
jujang <jeongookjang@naver.com>
2024-02-01 16:24:47 +0900

    Algorithm: HackerRank - MinimumDistances in Implementation problem solved

---
+++ 2_HackerRank/Implementation/MinimumDistances/src/Solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+        // Write your code here
+        int minDis = 1000;
+        for(int i = 0; i < a.size()-1; i++){
+            for(int j = i+1; j < a.size(); j++){
+                if(a.get(i).equals(a.get(j))){
+                    if(j - i > 0 && j - i < minDis) minDis = j - i;
+                }
+            }
+        }
+        if(minDis == 1000) return -1;
+        return minDis;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
