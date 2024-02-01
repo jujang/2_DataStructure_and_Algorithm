4fe0344a8a6e987434b7b3c873bbdc4c5e8a74cb
jujang <jeongookjang@naver.com>
2024-02-01 17:31:38 +0900

    Algorithm: HackerRank - HalloweenSale in Implementation problem solved

---
+++ 2_HackerRank/Implementation/HalloweenSale/src/Solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class Result {
+
+    /*
+     * Complete the 'howManyGames' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER d
+     *  3. INTEGER m
+     *  4. INTEGER s
+     */
+
+    public static int howManyGames(int p, int d, int m, int s) {
+        // Return the number of games you can buy
+        int sum = 0;
+        int cnt = 0;
+        int curP = p;
+        while(true){
+            if(sum + curP <= s) {
+                sum += curP;
+                cnt++;
+            } else {
+                break;
+            }
+            if(curP-d >= m) curP -= d;
+            else if(curP != m && curP-d < m) curP = m;
+        }
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int p = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        int m = Integer.parseInt(firstMultipleInput[2]);
+
+        int s = Integer.parseInt(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
